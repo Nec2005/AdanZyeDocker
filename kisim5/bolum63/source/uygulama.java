7e491b899383f8ac77db5b5e913a46c1280ac2e5
Nec2005 <necatiunal2005@gmail.com>
2021-12-27 12:13:00 +0000

    docker_test

--- kisim5/bolum63/source/uygulama.java
+++ kisim5/bolum63/source/uygulama.java
@@ -1,5 +1,5 @@
 public class uygulama {
     public static void main(String[] args) {
-        System.out.println("Merhaba ben bir java konsol uygulamasiyim");
+        System.out.println("Hello !! I am  a java console app...");
     }
 }
